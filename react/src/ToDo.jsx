15cc4f230fbc47010bfdaa8f0ed21135914bba83
MudadlaYogitha <322103312128@gvpce.ac.in>
2025-06-11 09:56:00 +0530

    to-do-list

--- react/src/ToDo.jsx
+++ react/src/ToDo.jsx
@@ -2,5 +2,60 @@ import React, { useState } from 'react';
 
 function ToDo(){
 
+    const [tasks,setTasks]=useState(["eat bf","bath",'walk']);
+    const [newTask,setNew]=useState("");
+
+    function handleInpChn(event){
+         setNew(event.target.value);
+    }
+    function addTask(){
+
+    }
+    function delTask(index){
+
+    }
+    function moveUp(index){
+
+    }
+    function moveDown(index){
+
+    }
+
+    return(<div className='to-do'>
+        <hi>To-Do-List</hi>
+        <div>
+            <input
+               type='text'
+               placeholder='enter task..'
+               value={newTask}
+               onChange={handleInpChn}/>
+               <button className='add-btn'
+               onClick={addTask}>
+                Add
+               </button>
+        </div>
+        <ol>
+            {tasks.map((task,index)=>
+                 <li key="index">
+                    <span className='text'>{task}</span>
+                    <button className='del-btn' 
+                    onClick={()=>delTask(index)}>
+                        Delete
+                    </button>
+                    <button className='up-btn' 
+                    onClick={()=>moveUp(index)}>
+                        👆
+                    </button>
+                    <button className='dwn-btn' 
+                    onClick={()=>moveDown(index)}>
+                        👇
+                    </button>
+                 </li>
+            )}
+        </ol>
+
+    </div>)
+
+
 }
 export default ToDo;
